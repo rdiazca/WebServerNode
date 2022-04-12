f38b085f2cfe8727dd55753ef792ab94cdfa2118
rdiazca <rdiazca96@gmail.com>
2022-04-12 00:31:44 -0400

    Cargar Express

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const port = 8080
+
+app.get('/',  (req, res) => {
+    res.send('Página principal')
+  });
+
+app.get('/hola-mundo',  (req, res) => {
+  res.send('Hola mundo en su respectiva ruta')
+});
+
+app.get('*',  (req, res) => {
+    res.send('404 | Page not found')
+  });
+
+app.listen(port, () =>{
+    console.log(`Example app listening at http://localhost:${port}`)
+});
